7d7e9c087a8dd2d8dae1690bf9be272ca6890c0f
guimurgel <guimurgel@gmail.com>
2019-08-28 18:50:00 -0300

    Bloqueando Queries e Mutations não autenticadas

---
+++ finances-back/src/utils.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+function getUserId (context) {
+
+  //"Authorization": "Bearer <token_jwt>"
+  const Authorization = context.request.get('Authorization')
+
+  //Verifica se Auth
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticated!')
+
+}
+
+module.exports = {
+  getUserId
+}
