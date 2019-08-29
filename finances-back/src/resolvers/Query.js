e220bf9c2b14743ab9064515a582465a78adba15
guimurgel <guimurgel@gmail.com>
2019-08-29 10:11:02 -0300

    Implementando Query para

--- finances-back/src/resolvers/Query.js
+++ finances-back/src/resolvers/Query.js
@@ -44,6 +44,38 @@ function categories (_, { operation }, ctx, info) {
 
 }
 
+function records (_, { type, accountsIds, categoriesIds }, ctx, info) {
+
+  const userId = getUserId(ctx)
+
+  let AND = [ { user: { id: userId } } ]
+
+  //Verifica se tem Operation para filtrar
+  AND = !type ? AND : [ ...AND, { type } ]
+
+  //Verifica se tem Accounts para filtrar
+  AND = !accountsIds || accountsIds.length === 0
+    ? AND
+    : [
+      ...AND,
+      { OR: accountsIds.map(id => ({ account: { id } })) }
+    ]
+  
+  //Verifica se tem Categories para filtrar
+  AND = !categoriesIds || categoriesIds.length === 0
+    ? AND
+    : [
+      ...AND,
+      { OR: categoriesIds.map(id => ({ category: { id } })) }
+    ]
+
+  return ctx.db.query.records({
+    where: { AND },
+    orderBy: "date_ASC"
+  }, info)
+
+}
+
 function user (_, args, ctx, info) {
 
   const userId = getUserId(ctx)
@@ -54,5 +86,6 @@ function user (_, args, ctx, info) {
 module.exports = {
   accounts,
   categories,
+  records,
   user
 }
