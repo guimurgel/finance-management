9f84ab5969d7a6d8fcd6efcbf8e261ebe1fe1e07
guimurgel <guimurgel@gmail.com>
2019-08-30 15:19:02 -0300

    Login Component: dimensão e

--- finances-front/src/App.vue
+++ finances-front/src/App.vue
@@ -1,23 +1,7 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
@@ -28,6 +12,6 @@ export default {
   name: 'App',
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
