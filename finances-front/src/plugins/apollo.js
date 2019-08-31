24fd9e64227e4e2d00bb5b8f30d7ad836eb36314
guimurgel <guimurgel@gmail.com>
2019-08-31 11:41:50 -0300

    Salvando token JWT no Local Storage

--- finances-front/src/plugins/apollo.js
+++ finances-front/src/plugins/apollo.js
@@ -1,5 +1,24 @@
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost'
 
+const AUTH_TOKEN = 'apollo-token'
+
+//Reseta o cache do Apollo Client
+cosnt resetApolloClient = async apollo => {
+  try {
+    await apollo.reseStore
+  } catch (e) {
+    console.log('%cError on cache reset', 'color: orange', error.message)
+  }
+}
+
+//Seta Token on Login
+const onLogin = async (apollo, token) => {
+  if (typeof window.localStorage !== 'undefined' && token) {
+    window.localStorage.setItem(AUTH_TOKEN, token)
+  }
+  await resetApolloClient(apollo)
+}
+
 const link = new HttpLink({
   uri: 'http://localhost:4000'
 })
@@ -11,3 +30,7 @@ const apollo = new ApolloClient({
 })
 
 export default apollo
+export {
+  AUTH_TOKEN,
+  onLogin,
+}
