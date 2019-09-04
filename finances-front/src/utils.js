50e55daa84d96c86de74af3a1da68aa7a9e74e99
guimurgel <guimurgel@gmail.com>
2019-09-04 11:34:43 -0300

    Função para agrupar registros com reduce

--- finances-front/src/utils.js
+++ finances-front/src/utils.js
@@ -21,6 +21,20 @@ const formatError = message => {
   return messageSplit[messageSplit.length - 1].trim()
 }
 
+// Agrupar por algum parametro
+const groupBy = (array, key, makeCurrentKey) => {
+  return array.reduce((accumulated, item) => {
+    const currentKey = makeCurrentKey(item, key)
+    return {
+      ...accumulated,
+      [currentKey]: [
+        ...(accumulated[currentKey] || []),
+        item
+      ]
+    }
+  }, {})
+}
+
 // Internacionalização de moedas
 const currencyFormatter = ({ locale, currency } = { locale: 'pt-BR', currency: 'BRL' }) => {
   return new Intl.NumberFormat(locale, {
@@ -31,6 +45,7 @@ const currencyFormatter = ({ locale, currency } = { locale: 'pt-BR', currency: '
 
 export {
   currencyFormatter,
+  groupBy,
   errorHandler,
   formatError
 }
