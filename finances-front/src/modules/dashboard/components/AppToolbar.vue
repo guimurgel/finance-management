d1bfb23a0bd52a50eb43d5af158199a5ccaa6c0e
guimurgel <guimurgel@gmail.com>
2019-09-05 06:57:49 -0300

    Vuex: State, Action e Mutation para titulo das páginas

--- finances-front/src/modules/dashboard/components/AppToolbar.vue
+++ finances-front/src/modules/dashboard/components/AppToolbar.vue
@@ -6,11 +6,14 @@
     color="primary"
   >
     <v-app-bar-nav-icon @click.stop="$emit('hide', !show)"></v-app-bar-nav-icon>
-    <v-toolbar-title> Dashboard </v-toolbar-title>
+    <v-toolbar-title> {{ title || 'Dashboard' }} </v-toolbar-title>
   </v-app-bar>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
   name: 'AppToolbar',
   props: {
@@ -19,6 +22,9 @@ export default {
   model: {
     prop: 'show',
     event: 'hide'
+  },
+  computed: {
+    ...mapState(['title'])
   }
 }
 </script>
