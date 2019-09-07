edd5f428c5a38011ef04b571c73b6e0ed12b47cc
guimurgel <guimurgel@gmail.com>
2019-09-07 07:17:23 -0300

    Apollo Cache: recalculando Query totalBalance

--- finances-front/src/modules/dashboard/modules/finances/services/records-service.js
+++ finances-front/src/modules/dashboard/modules/finances/services/records-service.js
@@ -12,6 +12,7 @@ const createRecord = async variables => {
     mutation: RecordCreateMutation,
     variables,
     update: (proxy, { data: { createRecord } }) => {
+      // RECORDS
       const month = moment(createRecord.date.substr(0, 10)).format('MM-YYYY')
       const variables = { month }
 
@@ -33,6 +34,33 @@ const createRecord = async variables => {
         })
       } catch (e) {
         console.log('Query "Records" has not been read yet!', e)
+      }
+
+      // TOTAL BALANCE
+      try {
+        const currentDate = moment().endOf('day')
+        const recordDate = moment(createRecord.date.substr(0, 10))
+        const variables = { date: currentDate.format('YYYY-MM-DD') }
+
+        if (recordDate.isBefore(currentDate)) {
+          // Leitura da query
+          const totalBalanceData = proxy.readQuery({
+            query: TotalBalanceQuery,
+            variables
+          })
+
+          // Inserindo novo valor
+          totalBalanceData.totalBalance = +(totalBalanceData.totalBalance + createRecord.amount).toFixed(2)
+
+          // Reescrevendo a query com os novos parametro
+          proxy.writeQuery({
+            query: TotalBalanceQuery,
+            variables,
+            data: totalBalanceData
+          })
+        }
+      } catch (e) {
+        console.log('Query "otalBalance" has not been read yet!', e)
       }
     }
   })
