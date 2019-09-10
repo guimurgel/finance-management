f03fa801a652dc260d0974c3ed7f90ca0ac078ce
guimurgel <guimurgel@gmail.com>
2019-09-10 13:36:50 -0300

    Configurando CORS e desabilitando playground em produção

---
+++ finances-back/src/config.js
@@ -0,0 +1,22 @@
+const { env } = process
+
+const isProduction = env.NODE_ENV === 'production'
+
+const endpoint = `${env.PRISMA_ENDPOINT}/${env.PRISMA_SERVICE}/${env.PRISMA_STAGE}`
+const secret = env.PRISMA_MANAGEMENT_API_SECRET
+const playground = isProduction ? false : '/'
+
+const corsHosts = env.CLIENT_HOSTS || ['localhost:8000', '127.0.0.1:8000'].join('|')
+const corsProtocols = isProduction ? 'wss|https' : 'ws|http'
+const origin = new RegExp(`(${corsProtocols}):\/\/(${corsHosts})(.+)?`)
+console.log(origin)
+//(wss|https):\/\/(localhost:8080|localhost:8081).(.+)?
+
+module.exports = {
+  endpoint,
+  env,
+  isProduction,
+  origin,
+  playground,
+  secret
+}
